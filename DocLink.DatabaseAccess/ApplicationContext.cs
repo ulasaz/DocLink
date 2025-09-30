ec6d25ccfe7c8d3cc1c1474e2d30ee4fe20b3c10
ulasaz <283005@student.pwr.edu.pl>
2025-09-30 14:04:53 +0200

    Initialized ApplicationContext and applied migrations (#13)

---
+++ DocLink.DatabaseAccess/ApplicationContext.cs
@@ -0,0 +1,52 @@
+using DocLink.Core.Models;
+using DocLink.Core;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocLink.Data;
+
+public class ApplicationContext : IdentityDbContext<Account, IdentityRole<Guid>, Guid>
+{
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Specialist> Specialists { get; set; }
+    public DbSet<Patient> Patients { get; set; }
+    public DbSet<Offer> Offers { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options) { }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.Entity<Specialist>().ToTable("Specialists");
+        builder.Entity<Patient>().ToTable("Patients");
+
+        builder.Entity<Patient>()
+            .HasMany(p => p.Reviews)
+            .WithOne(r => r.Patient)
+            .HasForeignKey(r => r.PatientId);
+        
+        builder.Entity<Patient>()
+            .HasMany(p => p.Appointments)
+            .WithOne(a => a.Patient)
+            .HasForeignKey(a => a.PatientId);
+        
+        builder.Entity<Specialist>()
+            .HasMany(s => s.Appointments)
+            .WithOne(a => a.Specialist)
+            .HasForeignKey(a => a.SpecialistId);
+        
+        builder.Entity<Specialist>()
+            .HasMany(s => s.Reviews)
+            .WithOne(r => r.Specialist)
+            .HasForeignKey(r => r.SpecialistId);
+        
+        builder.Entity<Specialist>()
+            .HasMany(s => s.Offers)
+            .WithOne(o => o.Specialist)
+            .HasForeignKey(o => o.SpecialistId);
+    }
+    
+}
